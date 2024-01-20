e06cecabe0331ade3d1c00b9ea7fe39894515e45
Your Name <haijamomen4@gmail.com>
2024-01-20 18:56:44 +0300

    Add project files.

---
+++ EF/Data/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿
+
+using EF.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.Data
+{
+    public class AppDbContext:DbContext 
+    {
+        public AppDbContext()
+        {
+
+
+        }
+        public AppDbContext(DbContextOptions options):base(options) 
+        {
+         
+            
+        }
+        public DbSet<Category> categories { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //if (!optionsBuilder.IsConfigured)
+            //{
+            //    optionsBuilder.UseSqlServer("Server=DESKTOP-2848GVF;Database=APiRepositry;Trusted_Connection=True;Integrated Security=true;Encrypt=False;");
+            //}
+            base.OnConfiguring(optionsBuilder);
+        }
+   
+    }
+}
---
+++ REpositryPattern/Program.cs
@@ -0,0 +1,17 @@
+using EF.Data;
+using Microsoft.EntityFrameworkCore;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("MyConnection")));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
